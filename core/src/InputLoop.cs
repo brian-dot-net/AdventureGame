1dfb74dec40028d7ea81f359223d051de9685cfe
Brian Rogers <bobbymcr@gmail.com>
2018-12-03 21:05:53 -0800

    InputLoop - move to top level type

---
+++ core/src/InputLoop.cs
@@ -0,0 +1,46 @@
+﻿// <copyright file="InputLoop.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+
+namespace Adventure
+{
+    using System;
+    using System.Threading;
+
+    public sealed class InputLoop : IDisposable
+    {
+        private readonly Func<bool> readInput;
+        private readonly IDisposable sub;
+
+        public InputLoop(MessageBus bus, Func<bool> readInput, Action<OutputMessage> onOutput)
+        {
+            this.readInput = readInput;
+            this.sub = bus.Subscribe(onOutput);
+        }
+
+        public void Run(CancellationToken token)
+        {
+            try
+            {
+                this.RunInner(token);
+            }
+            catch (OperationCanceledException)
+            {
+            }
+        }
+
+        public void Dispose()
+        {
+            this.sub.Dispose();
+        }
+
+        private void RunInner(CancellationToken token)
+        {
+            do
+            {
+                token.ThrowIfCancellationRequested();
+            }
+            while (this.readInput());
+        }
+    }
+}
--- core/src/TextConsole.cs
+++ core/src/TextConsole.cs
@@ -40,42 +40,5 @@ namespace Adventure
             this.bus.Send(new InputMessage(line));
             return true;
         }
-
-        private sealed class InputLoop : IDisposable
-        {
-            private readonly Func<bool> readInput;
-            private readonly IDisposable sub;
-
-            public InputLoop(MessageBus bus, Func<bool> readInput, Action<OutputMessage> onOutput)
-            {
-                this.readInput = readInput;
-                this.sub = bus.Subscribe(onOutput);
-            }
-
-            public void Run(CancellationToken token)
-            {
-                try
-                {
-                    this.RunInner(token);
-                }
-                catch (OperationCanceledException)
-                {
-                }
-            }
-
-            public void Dispose()
-            {
-                this.sub.Dispose();
-            }
-
-            private void RunInner(CancellationToken token)
-            {
-                do
-                {
-                    token.ThrowIfCancellationRequested();
-                }
-                while (this.readInput());
-            }
-        }
     }
 }
