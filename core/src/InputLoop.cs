d0fdf0a000565efd66fe2637931943444c2fd40c
Brian Rogers <bobbymcr@gmail.com>
2018-12-05 21:31:02 -0800

    InputLoop - RunReadTwoLines

--- core/src/InputLoop.cs
+++ core/src/InputLoop.cs
@@ -10,14 +10,22 @@ namespace Adventure
     public sealed class InputLoop : IDisposable
     {
         private readonly MessageBus bus;
+        private readonly IDisposable sub;
+
+        private bool inputEnded;
 
         public InputLoop(MessageBus bus)
         {
             this.bus = bus;
+            this.sub = bus.Subscribe<InputEndedMessage>(_ => this.inputEnded = true);
         }
 
         public void Run(CancellationToken token)
         {
+            while (!this.inputEnded)
+            {
+                this.bus.Send(new InputRequestedMessage());
+            }
         }
 
         public void Dispose()
--- core/test/InputLoopTest.cs
+++ core/test/InputLoopTest.cs
@@ -23,5 +23,25 @@ namespace Adventure.Test
                 act.Should().NotThrow();
             }
         }
+
+        [Fact]
+        public void RunReadTwoLines()
+        {
+            MessageBus bus = new MessageBus();
+            int linesRead = 0;
+            bus.Subscribe<InputRequestedMessage>(_ =>
+            {
+                if (++linesRead == 2)
+                {
+                    bus.Send(new InputEndedMessage());
+                }
+            });
+            using (InputLoop loop = new InputLoop(bus))
+            {
+                loop.Run(CancellationToken.None);
+
+                linesRead.Should().Be(2);
+            }
+        }
     }
 }
