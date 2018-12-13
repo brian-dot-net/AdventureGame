336e3996515ce38e7e1df17465f68c1be86deb90
Brian Rogers <bobbymcr@gmail.com>
2018-12-12 19:59:58 -0800

    Game - extract Table from MainRoom as a real Item

--- sample/src/MainRoom.cs
+++ sample/src/MainRoom.cs
@@ -6,58 +6,19 @@ namespace Adventure.Sample
 {
     internal sealed class MainRoom : Room
     {
-        private bool tableMoved;
-
         public MainRoom(MessageBus bus)
             : base(bus)
         {
+            this.Drop(Noun.Table, new Table(this));
         }
 
-        protected override string Description => "You are in the main room. There is a table here.";
+        protected override string Description => "You are in the main room.";
 
         protected override void EnterCore()
         {
             this.Register(Verb.Look, (_, n) => this.Look(n));
             this.Register(Verb.Greet, (_, __) => this.Output("You say, \"Hello,\" to no one in particular. No one answers."));
-            this.Register(Verb.Move, this.Move);
             this.Register(Verb.Take, this.Take);
-        }
-
-        protected override bool LookAtCore(Word noun)
-        {
-            if (noun.Primary == Noun.Table)
-            {
-                this.Output("It is an ordinary wooden table.");
-                return true;
-            }
-
-            return base.LookAtCore(noun);
-        }
-
-        private void Move(Word verb, Word noun)
-        {
-            if (noun.Primary == Noun.Table)
-            {
-                this.MoveTable();
-            }
-            else
-            {
-                this.Output($"You can't {verb} that.");
-            }
-        }
-
-        private void MoveTable()
-        {
-            if (!this.tableMoved)
-            {
-                this.Output("You move the table slightly. Underneath you see a coin.");
-                this.tableMoved = true;
-                this.Drop(Noun.Coin, new Coin());
-            }
-            else
-            {
-                this.Output("Someone has already moved it.");
-            }
         }
     }
 }
---
+++ sample/src/Table.cs
@@ -0,0 +1,47 @@
+﻿// <copyright file="Table.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+
+namespace Adventure.Sample
+{
+    internal sealed class Table : Item
+    {
+        private readonly Room parent;
+
+        private bool tableMoved;
+
+        public Table(Room parent)
+        {
+            this.parent = parent;
+        }
+
+        public override string ShortDescription => "a table";
+
+        public override string LongDescription => "It is an ordinary wooden table.";
+
+        protected override bool DoCore(MessageBus bus, Word verb, Word noun)
+        {
+            if (verb.Primary == Verb.Move)
+            {
+                this.Move(bus);
+                return true;
+            }
+
+            return base.DoCore(bus, verb, noun);
+        }
+
+        private void Move(MessageBus bus)
+        {
+            if (!this.tableMoved)
+            {
+                this.tableMoved = true;
+                bus.Send(new OutputMessage("You move the table slightly. Underneath you see a coin."));
+                this.parent.Drop(Noun.Coin, new Coin());
+            }
+            else
+            {
+                bus.Send(new OutputMessage("Someone has already moved it."));
+            }
+        }
+    }
+}
