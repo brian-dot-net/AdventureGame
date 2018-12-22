f13af46237f0299cd9f3b56930a056b603a83a9e
Brian Rogers <bobbymcr@gmail.com>
2018-12-22 11:03:05 -0800

    Item - NotTakenInitially

--- core/src/Item.cs
+++ core/src/Item.cs
@@ -19,6 +19,8 @@ namespace Adventure
 
         protected MessageBus Bus { get; }
 
+        protected bool Taken { get; private set; }
+
         public bool Do(Word verb, Word noun) => this.DoCore(verb, noun);
 
         public bool Take() => this.TakeCore();
---
+++ core/test/ItemTest.cs
@@ -0,0 +1,60 @@
+// <copyright file="ItemTest.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+
+namespace Adventure.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using Adventure.Messages;
+    using FluentAssertions;
+    using Xunit;
+
+    public sealed class ItemTest
+    {
+        [Fact]
+        public void NotTakenInitially()
+        {
+            MessageBus bus = new MessageBus();
+            List<string> output = new List<string>();
+            bus.Subscribe<OutputMessage>(m => output.Add(m.Text));
+            Item item = new TestItem(bus);
+
+            item.Do(new Word("use", "USE"), new Word("item", "ITEM"));
+
+            output.Should().ContainSingle().Which.Should().Be("You must take it to use it.");
+        }
+
+        private sealed class TestItem : Item
+        {
+            public TestItem(MessageBus bus)
+                : base(bus)
+            {
+            }
+
+            public override string ShortDescription => throw new NotImplementedException();
+
+            public override string LongDescription => throw new NotImplementedException();
+
+            protected override bool DoCore(Word verb, Word noun)
+            {
+                if (verb.Primary == "use")
+                {
+                    return this.Use();
+                }
+
+                return base.DoCore(verb, noun);
+            }
+
+            private bool Use()
+            {
+                if (!this.Taken)
+                {
+                    this.Output("You must take it to use it.");
+                }
+
+                return false;
+            }
+        }
+    }
+}
